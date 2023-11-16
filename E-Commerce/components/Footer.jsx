71472bd5772b9b690c29f907e447cd2d2e2d2fd1
Khanal Kshitiz <khanal.xitiz@gmail.com>
2023-11-16 11:53:32 +0200

    components are added

---
+++ E-Commerce/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="Customer-care">
+          <h1>Customer Care</h1>
+          <ul>
+            <li>Help center</li>
+            <li>How to buys</li>
+            <li>Return & Refund</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
